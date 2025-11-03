963be9dde3f4509f08f48469316577b4d22ca17d
DiegoCR25 <122698116+DiegoCR25@users.noreply.github.com>
2025-11-03 18:41:49 -0500

    Noticia de deportes agregada

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Listo para triunfar: Stefano Peschiera asegura estar en el mejor momento de su carrera",
+"autor" => "El Peruano",
+"resumen" => "A poco menos de un mes para que los Juegos Bolivarianos Ayacucho-Lima 2025 rompan fuegos, los deportistas nacionales ultiman sus entrenamientos para llegar en óptimas condiciones, como el medallista olímpico Stefano Peschiera, quien asegura que se encuentra en su mejor momento deportivo.
+
+Peschiera toma con seriedad su participación en el certamen bolivariano, que marca el inicio del nuevo ciclo olímpico, el cual concluye con los Juegos Los Ángeles 2028. Asegura que va por su segunda medalla de oro en la historia del torneo en la disciplina de la vela.",
 ];
 ?>
