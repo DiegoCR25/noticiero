12550e2423a2a2563927702905092f6bdf054690
DiegoCR25 <122698116+DiegoCR25@users.noreply.github.com>
2025-11-03 18:39:30 -0500

    Noticia de Nube agregada

--- api/secciones/nube.php
+++ api/secciones/nube.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nube = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Crecimiento de los servicios de nube pública representará más de 2.300 millones de dólares al 2027",
+"autor" => "El Peruano",
+"resumen" => "Los modelos operativos en la nube continuaron siendo el enfoque principal entre las empresas en Perú durante el 2022. Según los recientes estudios “IDC Semiannual Public Cloud Tracker, 2022H2” e “IDC Managed Coud Services Custom Report, 2022H2” de IDC Latin America, al cierre del 2022, el mercado de Cloud alcanzó los US$ 706.42 millones en Perú.",
 ];
 ?>
