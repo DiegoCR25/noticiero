c6b05403197f50beed037d1ea629d9e46dad38f5
DiegoCR25 <122698116+DiegoCR25@users.noreply.github.com>
2025-11-03 18:46:55 -0500

    Se agrego noticia de git y uns

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Telecomunicaciones: la ruta de GIT para desplegar US$ 25 millones en infraestructura",
+"autor" => "GESTIÓN",
+"resumen" => "Global Investing Towers (GIT), empresa peruana especializada en el desarrollo de infraestructura, como torres de telecomunicaciones, tiene como meta superar los S/ 50 millones en facturación. ¿Cómo logrará esta proyección? Entre sus planes destacan su internacionalización y una ruta de inversiones. Para conocer más sobre los objetivos a mediano plazo, Gestión conversó con su gerente general, Frank del Pino.",
 ];
 ?>
--- api/secciones/uns.php
+++ api/secciones/uns.php
@@ -4,8 +4,12 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $uns = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "ESTUDIANTES DE INGENIERÍA DE SISTEMAS Y BIOLOGÍA SE CAPACITAN CON EXPERTOS IINTERNACIONALES",
+"autor" => "Universidad Nacional del Santa",
+"resumen" => "Con el propósito de fortalecer su formación académica y potenciar sus competencias en investigación e innovación, estudiantes de las escuelas académicas de Ingeniería de Sistemas y Biología en Acuicultura de la Universidad Nacional del Santa (UNS) participaron en cursos internacionales de especialización dictados por expertos de la Universidad Autónoma de Chiriquí, Panamá.
+
+Estas capacitaciones se desarrollan en el marco del programa de subvenciones económicas de la UNS, iniciativa que impulsa el desarrollo de actividades académicas y de investigación formativa para estudiantes de pregrado y egresados, brindándoles acceso gratuito a experiencias internacionales de alto nivel.
+
+En el caso de los alumnos de Ingeniería de Sistemas, participaron del curso internacional “La ingeniería inversa de prompts e inteligencia híbrida para generar contenidos educativos, creación de imágenes y programación con herramientas de IA generativa”, dictado por el doctor Erick Miranda Patiño.",
 ];
 ?>
