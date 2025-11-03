7f7f466e969f87ac8bddc2f7104c49c3706e8478
DiegoCR25 <122698116+DiegoCR25@users.noreply.github.com>
2025-11-03 18:49:42 -0500

    Se agrego noticias de contenedores y clasificados

--- api/secciones/clasificados.php
+++ api/secciones/clasificados.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $clasificados = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Portal Clasificados.pe del Grupo El Comercio lanza nuevas opciones de avisos para inmuebles, vehículos y empleos desde S/ 100",
+  "autor" => "Redacción Economía",
+  "resumen" => "El sitio ofrece publicar anuncios para vivienda, autos, educación y servicios independientes con alcance a más de 100 000 personas. Esta iniciativa refuerza la presencia impresa-digital del grupo y facilita que particulares y empresas accedan a avisos clasificados desde montos accesibles.",
 ];
+
 ?>
--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Perú duplicará su tráfico de carga contenedorizada en los próximos 5 años gracias a nuevos terminales",
+  "autor" => "Redacción Logística",
+  "resumen" => "El país espera duplicar su tráfico de contenedores hacia 2030, impulsado por la puesta en marcha del megapuerto de Puerto de Chancay y reformas en la normativa de cabotaje. Según el informe, los terminales operativos y en desarrollo permitirán movilizar hasta 46,5 millones de toneladas métricas en 2025, un crecimiento estimado de 54 % sobre el ejercicio previo.",
 ];
+
 ?>
